38edd0cf416af13f3f104676bdb30df2f54238a2
Orinoco05 <bestadryze@gmail.com>
2023-09-26 20:17:04 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<h1 style="color:aliceblue">Document</h1>
+<h2>lmao</h2>
+<h3>okm</h3>
+</body>
+</html>
